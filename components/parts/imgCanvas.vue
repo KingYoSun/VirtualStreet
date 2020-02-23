99d0af69d7f1345babafecc0adee62c3fd2a89a3
KingYoSun <nocking257838@gmail.com>
2020-02-23 15:51:10 +0000

    Auto scaling image and crop it

---
+++ components/parts/imgCanvas.vue
@@ -0,0 +1,117 @@
+<template>
+  <div :id="'canvas-container-' + image.id" class="canvas-container">
+    <canvas :id="'canvas-' + image.id"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImgCanvas',
+  props: {
+    image: Object
+  },
+  data () {
+    return {
+      box: {
+        Top: 0,
+        Left: 0,
+        Width: 0,
+        Height: 0,
+        Right: 0,
+        Bottom: 0
+      },
+      width: 0,
+      height: 0
+    }
+  },
+  mounted () {
+    const self = this
+    const canvasContainer = document.getElementById('canvas-container-' + String(self.image.id))
+    const canvas = document.getElementById('canvas-' + String(self.image.id))
+    const ctx = canvas.getContext('2d')
+    const img = new Image()
+    img.src = self.image.url
+    img.onload = function () {
+      self.width = img.width
+      self.height = img.height
+      self.box = self.imageTrimming(self.image, self.width, self.height)
+      const drawGain = self.gain(canvasContainer)
+      self.resize(canvas, canvasContainer, self.box, drawGain)
+      ctx.drawImage(img, self.box.Left, self.box.Top, self.box.Width, self.box.Height, 0, 0, self.box.Width * drawGain, self.box.Height * drawGain)
+      self.$emit('sendHeight')
+    }
+  },
+  methods: {
+    gain (parent) {
+      const gain = parent.clientWidth / this.box.Width
+      const drawGain = (gain < 1.5) ? gain : 1.5
+      return drawGain
+    },
+    resize (canvas, parent, box, drawGain) {
+      const parentHeight = box.Height * drawGain
+      // parent.setAttribute('style', `height: ${parentHeight}px;`)
+      canvas.width = parent.clientWidth
+      canvas.height = parentHeight
+    },
+    imageTrimming (img, w, h) {
+      const boundingBox = this.mergeBoundingBox(img)
+      const left = Math.ceil(w * boundingBox.Left)
+      const top = Math.ceil(h * boundingBox.Top)
+      const width = Math.ceil(w * boundingBox.Width)
+      const height = Math.ceil(h * boundingBox.Height)
+      const mergedBox = {
+        Top: top,
+        Left: left,
+        Width: width,
+        Height: height
+      }
+      return mergedBox
+    },
+    mergeBoundingBox (img) {
+      const mergedBox = {
+        Top: 0,
+        Left: 0,
+        Width: 0,
+        Height: 0,
+        Right: 0,
+        Bottom: 0
+      }
+      mergedBox.Top = img.bounding_box[0].Top
+      mergedBox.Left = img.bounding_box[0].Left
+      mergedBox.Width = img.bounding_box[0].Width
+      mergedBox.Height = img.bounding_box[0].Height
+      for (const box of img.bounding_box) {
+        const boxRight = box.Left + box.Width
+        const boxBottom = box.Top + box.Height
+        mergedBox.Right = mergedBox.Left + mergedBox.Width
+        mergedBox.Bottom = mergedBox.Top + mergedBox.Height
+        if (mergedBox.Left > box.Left) {
+          mergedBox.Width = (mergedBox.Right < boxRight) ? box.Width : mergedBox.Width + mergedBox.Left - box.Left
+          mergedBox.Left = box.Left
+        } else {
+          // mergedBox.Left = mergedBox.Left
+          mergedBox.Width = (mergedBox.Right < boxRight) ? box.Width + box.Left - mergedBox.Left : mergedBox.Width
+        }
+        if (mergedBox.Top > box.Top) {
+          mergedBox.Height = (mergedBox.Bottom < boxBottom) ? box.Height : mergedBox.Height + mergedBox.Top - box.Top
+          mergedBox.Top = box.Top
+        } else {
+          // mergedBox.Top = mergedBox.Top
+          mergedBox.Height = (mergedBox.Bottom < boxBottom) ? box.Height + box.Top - mergedBox.Top : mergedBox.Height
+        }
+      }
+      return mergedBox
+    }
+  }
+}
+</script>
+
+<style>
+.canvas-container {
+  width: 100%;
+  height: auto;
+}
+canvas {
+  width: 100%;
+}
+</style>
--- components/parts/tweet.vue
+++ components/parts/tweet.vue
@@ -22,39 +22,29 @@
     <div ref="imagesContainer" class="images-container">
       <a :href="'https://twitter.com/' + tweet.user_screen_name + '/status/' + tweet.id" target="_blank">
         <div v-for="image in images" :key="image.id" class="each-image">
-          <img :id="'image-' + image.id" :src="image.url" @load="imageTrimming(image);setGridSpan(image.id)">
-          <div>{{ mergedBox }}</div>
+          <img-canvas :image="image" @sendHeight="recieveHeight" />
         </div>
       </a>
-    </div>
-    <div ref="imgView" class="imgView">
-      <span>{{ imgView }}</span>
     </div>
   </div>
 </template>
 
 <script>
 import TwitterIcon from '~/components/parts/twitterIcon.vue'
+import ImgCanvas from '~/components/parts/imgCanvas.vue'
 
 export default {
   name: 'EmbedTweet',
   components: {
-    TwitterIcon
+    TwitterIcon,
+    ImgCanvas
   },
   props: {
     tweet: Object
   },
   data () {
     return {
-      images: {},
-      imgView: {},
-      boxHeight: 0,
-      mergedBox: {
-        Top: 0,
-        Left: 0,
-        Width: 0,
-        Height: 0
-      }
+      images: {}
     }
   },
   mounted () {
@@ -62,74 +52,19 @@ export default {
       this.tweet.user_profile_image = 'https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png'
     }
     this.images = JSON.parse(this.tweet.img)
-    this.imgView = JSON.parse(this.tweet.img)
-    this.boxHeight = this.$refs.authorBox.offsetHeight + this.$refs.imgView.offsetHeight
     window.addEventListener('resize', this.setGridSpanResize)
   },
   beforeDestroy () {
     window.removeEventListener('resize', this.setGridSpanResize)
   },
   methods: {
-    setGridSpan ($id) {
-      const image = document.getElementById('image-' + String($id))
-      this.boxHeight += image.offsetHeight
-      const tweetContainer = document.getElementById('tweet-container-' + String(this.tweet.id))
-      tweetContainer.setAttribute('style', `height: ${this.boxHeight}px;grid-row: span ${Math.ceil(((this.boxHeight + 10) / (20 + 10)) + 1)};`)
+    recieveHeight () {
+      this.setGridSpanResize()
     },
     setGridSpanResize () {
-      const tweetBoxHeight = this.boxHeight + this.$refs.imagesContainer.offsetHeight
+      const tweetBoxHeight = this.$refs.authorBox.offsetHeight + this.$refs.imagesContainer.offsetHeight
       const tweetContainer = document.getElementById('tweet-container-' + String(this.tweet.id))
       tweetContainer.setAttribute('style', `height: ${tweetBoxHeight}px;grid-row: span ${Math.ceil(((tweetBoxHeight + 10) / (20 + 10)) + 1)};`)
-    },
-    imageTrimming (img) {
-      const size = this.getImageSize(img.url)
-      const boundingBox = this.mergeBoundingBox(img)
-      const left = Math.ceil(size.width * boundingBox.Left)
-      const top = Math.ceil(size.height * boundingBox.Top)
-      const width = Math.ceil(size.width * boundingBox.Width)
-      const height = Math.ceil(size.height * boundingBox.Height)
-      // const elemImage = document.getElementById('image-' + String(img.id))
-      this.mergedBox = {
-        Top: top,
-        Left: left,
-        Width: width,
-        Height: height
-      }
-    },
-    getImageSize (img) {
-      const size = {
-        width: 0,
-        height: 0
-      }
-      const element = new Image()
-      element.src = img
-      size.width = element.width
-      size.height = element.height
-      return size
-    },
-    mergeBoundingBox (img) {
-      const mergedBox = img.bounding_box[0]
-      for (const box of img.bounding_box) {
-        const boxRight = box.Left + box.Width
-        const mergedBoxRight = mergedBox.Left + mergedBox.Width
-        const boxBottom = box.Top + box.Height
-        const mergedBoxBottom = mergedBox.Top + mergedBox.Height
-        if (mergedBox.Left > box.Left) {
-          mergedBox.Left = box.Left
-          mergedBox.Width = (mergedBoxRight < boxRight) ? box.Width : mergedBox.Width + (mergedBox.Left - box.Left)
-        } else {
-          // mergedBox.Left = mergedBox.Left
-          mergedBox.Width = (mergedBoxRight < boxRight) ? box.Width + (box.Left - mergedBox.Left) : mergedBox.Width
-        }
-        if (mergedBox.Top > box.Top) {
-          mergedBox.Top = box.Top
-          mergedBox.Height = (mergedBoxBottom < boxBottom) ? box.Height : mergedBox.Height + (mergedBox.Top - box.Top)
-        } else {
-          // mergedBox.Top = mergedBox.Top
-          mergedBox.Height = (mergedBoxBottom < boxBottom) ? box.Height + (box.Top - mergedBox.Top) : mergedBox.Height
-        }
-      }
-      return mergedBox
     }
   }
 }
@@ -195,17 +130,5 @@ export default {
   height: 40px;
   min-width: 30px;
   min-height: 30px;
-}
-
-.images-container {
-  display: inline-block;
-}
-
-.each-image {
-  display: inline;
-}
-
-.each-image img {
-  width: 100%;
 }
 </style>
