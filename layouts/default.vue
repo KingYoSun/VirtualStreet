2ecee0548b92472a6760f831faa60b94555de734
KingYoSun <nocking257838@gmail.com>
2020-02-24 05:34:48 +0000

    Fix font-family

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,7 +27,8 @@ export default {
 
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+    Roboto, 'Helvetica Neue', '游ゴシック', YuGothic, 'メイリオ', Meiryo, 'ヒラギノ角ゴ ProN W3',
+    'Hiragino Kaku Gothic ProN', 'ＭＳ Ｐゴシック', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
